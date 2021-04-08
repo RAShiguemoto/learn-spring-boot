55e06d44adf4914741ab33e275c0142f263babe6
RAS <raulaguirreshiguemoto@gmail.com>
2021-04-07 23:20:43 -0300

    Criacao de metodo POST

--- anime/src/main/java/com/ras/anime/service/AnimeService.java
+++ anime/src/main/java/com/ras/anime/service/AnimeService.java
@@ -6,18 +6,24 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
-    private List<Anime> animes = List.of(
-            new Anime(1L, "Boku No Hero Academia", new BigDecimal("9.40")),
-            new Anime(2L, "Shingeki no Kyojin", new BigDecimal("9.50")),
-            new Anime(3L, "Naruto", new BigDecimal("10.00")),
-            new Anime(4L, "Bleach", new BigDecimal("9.50")),
-            new Anime(5L, "Katekyo Hitman Reborn", new BigDecimal("9.30")),
-            new Anime(6L, "Shokugeki No Souma", new BigDecimal("8.50")),
-            new Anime(7L, "One Punch Man", new BigDecimal("9.00")));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(
+                new Anime(1L, "Boku No Hero Academia", new BigDecimal("9.40")),
+                new Anime(2L, "Shingeki no Kyojin", new BigDecimal("9.50")),
+                new Anime(3L, "Naruto", new BigDecimal("10.00")),
+                new Anime(4L, "Bleach", new BigDecimal("9.50")),
+                new Anime(5L, "Katekyo Hitman Reborn", new BigDecimal("9.30")),
+                new Anime(6L, "Shokugeki No Souma", new BigDecimal("8.50")),
+                new Anime(7L, "One Punch Man", new BigDecimal("9.00"))));
+    }
 
     public List<Anime> listAll() {
         return animes;
@@ -29,5 +35,11 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime Not Found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(8, 100));
+        animes.add(anime);
+        return anime;
     }
 }
